cee50c9f8fbd20cd11dfe7d1cfba0930d273214b
wellsbabo <wellsbabo@naver.com>
2024-08-14 12:27:47 +0900

    init

---
+++ src/main/java/wellsbabo/spring_security_jwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package wellsbabo.spring_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtApplication.class, args);
+    }
+
+}
